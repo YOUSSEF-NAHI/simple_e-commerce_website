7618e88b1abdc64e8bd460323d88a78f384ab46d
Youssef Nahi <youssefnahi1998@gmail.com>
2022-01-15 18:10:40 +0100

    second commit

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $products = collect([1,2,3,4,5,6]);
+        return view('front.welcome', compact('products'));
+    }
+}
